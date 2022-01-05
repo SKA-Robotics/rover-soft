906f9a159a0ed61e1c3300d18886adec0ee82461
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-01-05 23:38:24 +0100

    make subscriber/publisher and (uncompleted) launch file

---
+++ src/joy_tank_steering/scripts/joy_tank_steering.py
@@ -0,0 +1,31 @@
+#!/usr/bin python3
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+left_right_axis_ind = 2
+front_back_axis_ind = 3
+max_linear_weels_velocity = 1 # to add
+max_angular_weels_velocity = 1 # to add
+linear_velocity = 0
+angular_velocity = 0
+
+def callback(data)
+    linear_velocity = max_linear_weels_velocity * data.axes[front_back_axis_ind]
+    angular_velocity = max_angular_weels_velocity * data.axes[left_right_axis_ind]
+
+def talker():
+    rospy.init_node("tank_steering_node")
+
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+    rate = rospy.Rate(20) # 20Hz
+    while not rospy.is_shutdown():
+        rospy.Subscriber("joy", Joy, callback)
+        pub.publish([ linear_velocity, 0, 0], [0, 0, angular_velocity ])
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
