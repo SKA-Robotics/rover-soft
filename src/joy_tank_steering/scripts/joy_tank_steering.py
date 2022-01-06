00d36e0c47c5c93d9e1d95d8be54c67f5b8f5779
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-01-06 17:45:24 +0100

    update code

--- src/joy_tank_steering/scripts/joy_tank_steering.py
+++ src/joy_tank_steering/scripts/joy_tank_steering.py
@@ -10,18 +10,24 @@ max_angular_weels_velocity = 1 # to add
 linear_velocity = 0
 angular_velocity = 0
 
-def callback(data)
+def callback(data):
     linear_velocity = max_linear_weels_velocity * data.axes[front_back_axis_ind]
     angular_velocity = max_angular_weels_velocity * data.axes[left_right_axis_ind]
 
 def talker():
     rospy.init_node("tank_steering_node")
-
     pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-    rate = rospy.Rate(20) # 20Hz
+    rate = rospy.Rate(50) # 50Hz
     while not rospy.is_shutdown():
         rospy.Subscriber("joy", Joy, callback)
-        pub.publish([ linear_velocity, 0, 0], [0, 0, angular_velocity ])
+        msg = Twist()
+        msg.linear.x = linear_velocity
+        msg.linear.y = 0
+        msg.linear.x = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = angular_velocity
+        pub.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
