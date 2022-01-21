c96a62d8ff77a21b4f1432f979910908cb53cf35
Koglaza <Koglaza@hotmail.com>
2022-01-21 17:47:45 +0100

    Stan projketu kotroli lamp na 21.01- do dokończenia

--- src/ral_sirius_1/src/light_control.py
+++ src/ral_sirius_1/src/light_control.py
@@ -1,15 +1,20 @@
 import rospy
 import serial
+from std_msgs.msg import Int32
 
-
+def callback(data):
+     A=serial.Serial("/dev/ttyUSB1", 9600)
+     A.write(data.encode())
 def main():
     rospy.init_node("light_control") 
-    A=serial.Serial("/dev/ttyUSB1", 9600)   
-    A.write(ord("1")) 
+    #A=serial.Serial("/dev/ttyUSB1", 9600)   #parametr powinien byc wczytywany
+    rospy.Subscriber("status_light", Int32, callback)
     rospy.spin()
-   
 if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
         pass 
+#przerobic na obiektowe
+#serial rozpisac  by miec dostep wszedzie
+#nie inicjowac urzadzenia szeregowego za kazda utrzymana informacje
