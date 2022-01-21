620ad22b0c15623f4470d7e52e114dc67c36c843
Koglaza <Koglaza@hotmail.com>
2022-01-21 15:42:52 +0100

    praaca

---
+++ src/ral_sirius_1/src/light_control.py
@@ -0,0 +1,14 @@
+import rospy
+import serial
+
+def main():
+    rospy.init_node("light_control") 
+    A=serial.Serial("/dev/ttyUSB1", 9600)   
+    A.write(1)
+    rospy.spin()
+   
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.rospy.ROSInterruptException:
+        pass 
