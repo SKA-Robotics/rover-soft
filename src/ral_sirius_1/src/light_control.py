0a7e11491e33b6ed3a62b98e8e2d31d80c5cc255
Koglaza <Koglaza@hotmail.com>
2022-01-21 15:59:56 +0100

    Dodanie ord

--- src/ral_sirius_1/src/light_control.py
+++ src/ral_sirius_1/src/light_control.py
@@ -1,10 +1,11 @@
 import rospy
 import serial
 
+
 def main():
     rospy.init_node("light_control") 
     A=serial.Serial("/dev/ttyUSB1", 9600)   
-    A.write(1)
+    A.write(ord("1")) 
     rospy.spin()
    
 if __name__ == "__main__":
