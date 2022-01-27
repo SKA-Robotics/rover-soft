b27b79a58b3295670690b943d891f41f95901ffa
Koglaza <Koglaza@hotmail.com>
2022-01-27 23:37:21 +0100

    Adding second lamp

--- src/ral_sirius_1/src/light_control.py
+++ src/ral_sirius_1/src/light_control.py
@@ -6,17 +6,22 @@ class SerialDevice:
         self.parameter = parameter
         self.name = name
     def set_parameter(self):
-       self.name=serial.Serial(self.parameter, 9600)
+        self.name=serial.Serial(self.parameter, 9600)   
+    def send_byte(self):
+        self.name.write(ord("1"))
     def callback(self, data):
         self.name.write(data.encode())
 def main():
     rospy.init_node("light_control") 
     device = SerialDevice("/dev/ttyUSB1", "A")
+    starting_light = SerialDevice("/dev/ttyUSB2", "B")
     try:
         device.set_parameter()
+        starting_light.set_parameter()
     except KeyError:
         print("Parameter is not set")
         pass
+    starting_light.send_byte()
     rospy.Subscriber("status_light", Int32, device.callback)
     rospy.spin()
 if __name__ == "__main__":
