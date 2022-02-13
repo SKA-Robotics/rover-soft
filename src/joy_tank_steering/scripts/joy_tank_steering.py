a45cd2a8dc38ea5b6dc3491b5b65d1a9828deb8c
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-02-14 00:17:26 +0100

    Update

--- src/joy_tank_steering/scripts/joy_tank_steering.py
+++ src/joy_tank_steering/scripts/joy_tank_steering.py
@@ -6,27 +6,26 @@ from geometry_msgs.msg import Twist
 
 left_right_axis_ind = 3
 front_back_axis_ind = 4
-max_linear_weels_velocity = 1.0 # default
-max_angular_weels_velocity = 1.0 # default
-linear_velocity = 0.0
-angular_velocity = 0.0
 
-def callback(data: Joy):
-    global linear_velocity
-    global angular_velocity
+class Rover:
+    max_linear_weels_velocity = 1.0 # default
+    max_angular_weels_velocity = 1.0 # default
+    linear_velocity = 0.0
+    angular_velocity = 0.0
+
+def callback(data: Joy, rover: Rover):
     if data:
-        linear_velocity = max_linear_weels_velocity * data.axes[front_back_axis_ind]
-        angular_velocity = max_angular_weels_velocity * data.axes[left_right_axis_ind]
+        rover.linear_velocity = rover.max_linear_weels_velocity * data.axes[front_back_axis_ind]
+        rover.angular_velocity = rover.max_angular_weels_velocity * data.axes[left_right_axis_ind]
     else:
-        linear_velocity = 0.0
-        angular_velocity = 0.0
+        rover.linear_velocity = 0.0
+        rover.angular_velocity = 0.0
 
 def talker():
     rospy.init_node("tank_steering_node")
-    global max_linear_weels_velocity
-    global max_angular_weels_velocity
-    max_linear_weels_velocity = rospy.get_param("max_linear_vel", 1.0) # [m/s]
-    max_angular_weels_velocity = rospy.get_param("max_angular_vel", 1.0) # [rad/s]
+    rover = Rover
+    rover.max_linear_weels_velocity = rospy.get_param("max_linear_vel", 1.0) # [m/s]
+    rover.max_angular_weels_velocity = rospy.get_param("max_angular_vel", 1.0) # [rad/s]
     pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
     rate = rospy.Rate(50) # 50Hz
     msg = Twist()
@@ -36,10 +35,10 @@ def talker():
     msg.angular.x = 0
     msg.angular.y = 0
     msg.angular.z = 0
+    rospy.Subscriber("joy", Joy, callback)
     while not rospy.is_shutdown():
-        rospy.Subscriber("joy", Joy, callback)
-        msg.linear.x = linear_velocity
-        msg.angular.z = angular_velocity
+        msg.linear.x = rover.linear_velocity
+        msg.angular.z = rover.angular_velocity
         pub.publish(msg)
         rate.sleep()
     msg.linear.x = 0
