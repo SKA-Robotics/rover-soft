98498fdb9494cbe04b6f5a032c1f50ad6d2179fe
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-02-15 16:45:17 +0100

    Fix pipeline and update code

--- src/joy_tank_steering/scripts/joy_tank_steering.py
+++ src/joy_tank_steering/scripts/joy_tank_steering.py
@@ -7,23 +7,19 @@ from geometry_msgs.msg import Twist
 left_right_axis_ind = 3
 front_back_axis_ind = 4
 
-class Rover:
+class RoverMoves:
     max_linear_weels_velocity = 1.0 # default
     max_angular_weels_velocity = 1.0 # default
     linear_velocity = 0.0
     angular_velocity = 0.0
 
-def callback(data: Joy, rover: Rover):
-    if data:
-        rover.linear_velocity = rover.max_linear_weels_velocity * data.axes[front_back_axis_ind]
-        rover.angular_velocity = rover.max_angular_weels_velocity * data.axes[left_right_axis_ind]
-    else:
-        rover.linear_velocity = 0.0
-        rover.angular_velocity = 0.0
+def joy_callback(data: Joy, rover: RoverMoves):
+    rover.linear_velocity = rover.max_linear_weels_velocity * data.axes[front_back_axis_ind]
+    rover.angular_velocity = rover.max_angular_weels_velocity * data.axes[left_right_axis_ind]
 
-def talker():
+def joy_talker():
     rospy.init_node("tank_steering_node")
-    rover = Rover
+    rover = RoverMoves()
     rover.max_linear_weels_velocity = rospy.get_param("max_linear_vel", 1.0) # [m/s]
     rover.max_angular_weels_velocity = rospy.get_param("max_angular_vel", 1.0) # [rad/s]
     pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
@@ -35,7 +31,7 @@ def talker():
     msg.angular.x = 0
     msg.angular.y = 0
     msg.angular.z = 0
-    rospy.Subscriber("joy", Joy, callback)
+    rospy.Subscriber("joy", Joy, joy_callback, rover)
     while not rospy.is_shutdown():
         msg.linear.x = rover.linear_velocity
         msg.angular.z = rover.angular_velocity
@@ -47,6 +43,6 @@ def talker():
 
 if __name__ == '__main__':
     try:
-        talker()
+        joy_talker()
     except rospy.ROSInterruptException:
         pass
