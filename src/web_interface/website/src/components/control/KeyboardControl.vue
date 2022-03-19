149c37bf5f64cf7462201ed53300cfc26a0c3cba
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-03-19 20:18:32 +0100

    Add keyboard reading and publisher

--- src/web_interface/website/src/components/control/KeyboardControl.vue
+++ src/web_interface/website/src/components/control/KeyboardControl.vue
@@ -1,28 +1,30 @@
 <template>
     <div>
-        <label>
-            Keyboard Rover Steering
-        </label>
-    </div>
-    <div>
-        <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" id="linear_speed" v-model="linear_speed_percentage">
-            <label class="sliderLabel">Linear velocity: {{ this.linear_speed_percentage }}%</label>
+        <div>
+            <label>
+                Keyboard Rover Steering
+            </label>
         </div>
-        <div class="slidecontainer">
-            <input type="range" min="1" max="100" value="50" class="slider" id="angular_speed" v-model="angular_speed_percentage">
-            <label class="sliderLabel">Angular velocity: {{ this.angular_speed_percentage }}%</label>
+        <div>
+            <div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="linear_speed" v-model="linear_speed_percentage">
+                <label class="sliderLabel">Linear velocity: {{ this.linear_speed_percentage }}%</label>
+            </div>
+            <div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="angular_speed" v-model="angular_speed_percentage">
+                <label class="sliderLabel">Angular velocity: {{ this.angular_speed_percentage }}%</label>
+            </div>
         </div>
-    </div>
-    <div class="keyboardControl">
-        <p>
-            <button class="keyboardControl" @press="goForward">W</button>
-        </p>
-        <p>
-            <button class="keyboardControl" @press="turnLeft">A</button>
-            <button class="keyboardControl" @press="goBackward">S</button>
-            <button class="keyboardControl" @press="turnRight">D</button>
-        </p>
+        <div class="keyboardControl">
+            <p class="keyboardControl">
+                <button class="keyboardControl" @press="goForward">W</button>
+            </p>
+            <p class="keyboardControl">
+                <button class="keyboardControl" @press="turnLeft">A</button>
+                <button class="keyboardControl" @press="goBackward">S</button>
+                <button class="keyboardControl" @press="turnRight">D</button>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -40,20 +42,60 @@
                 max_angular_speed: 1.57,
                 linear_speed_percentage: 25,
                 angular_speed_percentage: 25,
+                timer: null,
+                pressed_W: false,
+                pressed_A: false,
+                pressed_S: false,
+                pressed_D: false,
             }
         },
         methods: {
-            goForward() {
-
+            goForward() {this.pressed_W = true;},
+            goBackward() {this.pressed_S = true;},
+            turnLeft() {this.pressed_A = true;},
+            turnRight() {this.pressed_D = true;},
+            stopForward() {this.pressed_W = false;},
+            stopBackward() {this.pressed_S = false;},
+            stopLeft() {this.pressed_A = false;},
+            stopRight() {this.pressed_D = false;},
+            startPublishing() {
+                this.timer = setInterval(() => {
+                    var message = new window.ROSLIB.Message({
+                        linear : {
+                        x : ( this.pressed_W - this.pressed_S ) * this.max_linear_speed * 0.01 * this.linear_speed_percentage,
+                        y : 0,
+                        z : 0
+                        },
+                        angular : {
+                        x : 0,
+                        y : 0,
+                        z : ( this.pressed_A - this.pressed_D ) * this.max_angular_speed * 0.01 * this.angular_speed_percentage
+                        }
+                    });
+                    this.topic.publish(message);
+                    this.stopForward();
+                    this.stopBackward();
+                    this.stopLeft();
+                    this.stopRight();
+                }, 20) // message rate: 20 ms
             },
-            goBackward() {
-
-            },
-            turnLeft() {
-
-            },
-            turnRight() {
-
+            keyListener(e) {
+                switch (e.key) {
+                    case 'w':
+                        this.goForward();
+                        break;
+                    case 's':
+                        this.goBack();
+                        break;
+                    case 'a':
+                        this.turnLeft();
+                        break;
+                    case 'd':
+                        this.turnRight();
+                        break;
+                    default:
+                        break;
+                }
             }
         },
         mounted() {
@@ -62,6 +104,11 @@
                 name : '/cmd_vel',
                 messageType : 'geometry_msgs/Twist'
             });
+            window.addEventListener('keypress', this.keyListener);
+        },
+        beforeDestroy() {
+            clearInterval(this.timer);
+            window.removeEventListener('keypress', this.keyListener);
         }
     };
 </script>
