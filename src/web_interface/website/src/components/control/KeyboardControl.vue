bdc4be282bbee52e67632eb39ae23f3c3460942d
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-04-02 19:01:34 +0200

    Fix problems

--- src/web_interface/website/src/components/control/KeyboardControl.vue
+++ src/web_interface/website/src/components/control/KeyboardControl.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
         <div>
-            <label>
+            <h2>
                 Keyboard Rover Steering
-            </label>
+            </h2>
         </div>
         <div>
             <div class="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="linear_speed" v-model="linear_speed_percentage">
+                <input type="range" min="1" max="100" class="slider" id="linear_speed" v-model="linear_speed_percentage">
                 <label class="sliderLabel">Linear velocity: {{ this.linear_speed_percentage }}%</label>
             </div>
             <div class="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="angular_speed" v-model="angular_speed_percentage">
+                <input type="range" min="1" max="100" class="slider" id="angular_speed" v-model="angular_speed_percentage">
                 <label class="sliderLabel">Angular velocity: {{ this.angular_speed_percentage }}%</label>
             </div>
         </div>
@@ -25,10 +25,12 @@
                 <button class="keyboardControl" :class="{ pressed: pressed_D }">D</button>
             </p>
         </div>
+        <p>Use 'Space' to switch between sliders or 'Arrow Keys' to change each speed value.</p> 
     </div>
 </template>
 
 <script>
+import { capitalize } from '@vue/shared';
     export default {
         name: "KeyboardControl",
         props: {
@@ -48,6 +50,8 @@
                 pressed_A: false,
                 pressed_S: false,
                 pressed_D: false,
+                elements: ["linear_speed", "angular_speed"],
+                focus_index: 0
             }
         },
         methods: {
@@ -69,7 +73,8 @@
                 }, this.message_rate)
             },
             keyListener(e, isPressed) {
-                switch (e.key) {
+                var key = capitalize(e.key);
+                switch (key) {
                     case 'W':
                         this.pressed_W = isPressed;
                         break;
@@ -82,6 +87,11 @@
                     case 'D':
                         this.pressed_D = isPressed;
                         break;
+                    case ' ':
+                        if(isPressed)
+                            this.focus_index = (this.focus_index + 1) % this.elements.length;
+                        document.getElementById(this.elements[this.focus_index]).focus();
+                        break;
                     default:
                         break;
                 }
@@ -93,34 +103,38 @@
                 name : '/cmd_vel',
                 messageType : 'geometry_msgs/Twist'
             });
-            window.addEventListener('keydown', this.keyListener($event, true));
-            window.addEventListener('keyup', this.keyListener($event, false));
-            startPublishing();
+            window.addEventListener('keydown', event => {
+                this.keyListener(event, true);
+            });
+            window.addEventListener('keyup', event => {
+                this.keyListener(event, false);
+            });
+            this.startPublishing();
+            document.getElementById(this.elements[this.focus_index]).focus();
         },
         beforeDestroy() {
             clearInterval(this.timer);
-            window.removeEventListener('keydown', this.keyListener($event, true));
-            window.removeEventListener('keyup', this.keyListener($event, false));
+            window.removeEventListener('keydown');
+            window.removeEventListener('keyup');
         }
     };
 </script>
 
 <style>
     button.keyboardControl {
-        margin: 5px;
-        width: 50px;
-        height: 50px;
-        font-size: large;
-        background-color: #ddd;
+        margin: 8px;
+        width: 80px;
+        height: 80px;
+        font-size: 40px;
     }
     button.keyboardControl.pressed {
-        background-color: #999;
+        background-color: var(--secondary-light);
     }
     div.keyboardControl {
         padding: 10px;
         text-align: center;
         background: #eee;
-        margin: 0;
+        margin: 50px;
         display: inline-block;
         align-items: center;
     }
