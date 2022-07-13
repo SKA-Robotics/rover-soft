f9eeed402237f601fee5390d3b276ce348d7960c
Mateusz533 <94320506+Mateusz533@users.noreply.github.com>
2022-07-13 19:47:05 +0200

    Make data structure clearer

--- src/web_interface/website/src/components/control/ManipKeyboardControl.vue
+++ src/web_interface/website/src/components/control/ManipKeyboardControl.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="manipKeyboardControl">
-        <div v-for:="i in this.elements_names.length" class="slidecontainer">
+        <div v-for:="(element, i) in this.elements" class="slidecontainer">
             <td class="col1">
-                <input type="range" min="1" max="100" class="slider" :class="{ focused: focus_index == i - 1 }"
-                :id="elements_names[i - 1]" v-model="this.elements_effort_percentage[i - 1]" v-on:input="sliderInputCallback"
-                @click="focus_index = i - 1">
+                <input type="range" min="1" max="100" class="slider" :class="{ focused: focus_index == i }"
+                :id="element.name" v-model="element.effort_percentage" v-on:input="sliderInputCallback"
+                @click="focus_index = i">
             </td>
             <td class="col2">
-                <label class="sliderLabel">{{ this.elements_text[i - 1] }}:</label>
+                <label class="sliderLabel">{{ element.text }}:</label>
             </td>
             <td class="col3">
-                <label class="sliderLabel">{{ this.elements_effort_percentage[i - 1] }}%</label>
+                <label class="sliderLabel">{{ element.effort_percentage }}%</label>
             </td>
         </div>
         <div class="keyboardBox">
@@ -25,10 +25,10 @@
                 <button :class="{ pressed: pressed_D }">D</button>
             </p>
         </div>
-        
+
         <p>Use 'Space' to switch between sliders or 'Arrow Keys' to change each effort value.</p>
         <p>Steering will be automaticly switched between the arm or the claw by changing the choosen slider. Use keys:</p>
-        <div class="keyboardBox">
+        <div class="description">
             <li><b>'A'</b> and <b>'D'</b> to rotate the arm or the claw</li>
             <li><b>'W'</b> and <b>'S'</b> to lift the arm or the claw</li>
             <li><b>'Q'</b> and <b>'E'</b> to tilt the arm or clamp the claw</li>
@@ -56,11 +56,15 @@ import { capitalize } from '@vue/shared';
                 pressed_A: false,
                 pressed_S: false,
                 pressed_D: false,
-                elements_effort_percentage: [25, 25, 25, 25, 25, 25],
-                elements_text: ["Arm rotate effort", "Arm lift effort", "Arm tilt effort", "Claw rotate effort", "Claw lift effort", "Claw clamp effort"],
-                elements_names: ["arm_rotate", "arm_lift", "arm_tilt", "claw_rotate", "claw_lift", "claw_clamp"],
-                cookies_names: ["arm-rotate", "arm-lift", "arm-tilt", "claw-rotate", "claw-lift", "claw-clamp"],
-                focus_index: 0
+                focus_index: 0,
+                elements: [
+                    { text: "Arm rotate effort", name: "arm_rotate", cookie: "arm-rotate", effort_percentage: 25 },
+                    { text: "Arm lift effort", name: "arm_lift", cookie: "arm-lift", effort_percentage: 25 },
+                    { text: "Arm tilt effort", name: "arm_tilt", cookie: "arm-tilt", effort_percentage: 25 },
+                    { text: "Claw rotate effort", name: "claw_rotate", cookie: "claw-rotate", effort_percentage: 25 },
+                    { text: "Claw lift effort", name: "claw_lift", cookie: "claw-lift", effort_percentage: 25 },
+                    { text: "Claw clamp effort", name: "claw_clamp", cookie: "claw-clamp", effort_percentage: 25 },
+                ]
             }
         },
         methods: {
@@ -78,14 +82,14 @@ import { capitalize } from '@vue/shared';
                         effort : [ 0, 0, 0, 0, 0, 0 ]
                     });
                     if( this.focus_index >= 0 && this.focus_index <= 2 ){
-                        message.effort[0] = ( this.pressed_A - this.pressed_D ) * this.max_effort * 0.01 * elements_effort_percentage[0];
-                        message.effort[1] = ( this.pressed_W - this.pressed_S ) * this.max_effort * 0.01 * elements_effort_percentage[1];
-                        message.effort[2] = ( this.pressed_Q - this.pressed_E ) * this.max_effort * 0.01 * elements_effort_percentage[2];
+                        message.effort[0] = ( this.pressed_A - this.pressed_D ) * this.max_effort * 0.01 * elements[0].effort_percentage;
+                        message.effort[1] = ( this.pressed_W - this.pressed_S ) * this.max_effort * 0.01 * elements[1].effort_percentage;
+                        message.effort[2] = ( this.pressed_Q - this.pressed_E ) * this.max_effort * 0.01 * elements[2].effort_percentage;
                     }
                     else if( this.focus_index >= 3 && this.focus_index <= 5 ){
-                        message.effort[3] = ( this.pressed_A - this.pressed_D ) * this.max_effort * 0.01 * elements_effort_percentage[3];
-                        message.effort[4] = ( this.pressed_W - this.pressed_S ) * this.max_effort * 0.01 * elements_effort_percentage[4];
-                        message.effort[5] = ( this.pressed_Q - this.pressed_E ) * this.max_effort * 0.01 * elements_effort_percentage[5];
+                        message.effort[3] = ( this.pressed_A - this.pressed_D ) * this.max_effort * 0.01 * elements[3].effort_percentage;
+                        message.effort[4] = ( this.pressed_W - this.pressed_S ) * this.max_effort * 0.01 * elements[4].effort_percentage;
+                        message.effort[5] = ( this.pressed_Q - this.pressed_E ) * this.max_effort * 0.01 * elements[5].effort_percentage;
                     }
                     this.topic.publish(message);
                 }, this.message_rate)
@@ -113,16 +117,16 @@ import { capitalize } from '@vue/shared';
                         break;
                     case ' ':
                         if(isPressed)
-                            this.focus_index = (this.focus_index + 1) % this.elements_names.length;
-                        document.getElementById(this.elements_names[this.focus_index]).focus();
+                            this.focus_index = (this.focus_index + 1) % this.elements.length;
+                        document.getElementById(this.elements[this.focus_index].name).focus();
                         break;
                     default:
                         break;
                 }
             },
             sliderInputCallback() {
-                for(i in this.elements_names.length) {
-                    this.$cookies.set(this.cookies_names[i - 1], this.elements_effort_percentage[i - 1]);
+                for(i in this.elements.length) {
+                    this.$cookies.set(this.elements[i - 1].cookie, this.elements[i - 1].effort_percentage);
                 }
             }
         },
@@ -141,8 +145,8 @@ import { capitalize } from '@vue/shared';
 
             // Read previous percentage settings
             for(i in this.elements_names.length) {
-                if (this.$cookies.isKey(this.cookies_names[i - 1])) {
-                    this.elements_effort_percentage[i - 1] = this.$cookies.get(this.cookies_names[i - 1]);
+                if (this.$cookies.isKey(this.elements[i - 1].cookie)) {
+                    this.elements[i - 1].effort_percentage = this.$cookies.get(this.elements[i - 1].cookie);
                 }
             }
 
@@ -160,7 +164,7 @@ import { capitalize } from '@vue/shared';
             this.startPublishing();
 
             // Set focus on first slider
-            document.getElementById(this.elements_names[this.focus_index]).focus();
+            document.getElementById(this.elements[this.focus_index].name).focus();
         },
         beforeDestroy() {
             clearInterval(this.timer);
