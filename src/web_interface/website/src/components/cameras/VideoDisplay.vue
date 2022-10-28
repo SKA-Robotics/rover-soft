7794d63d3db245ee066bcfa99da1fa33a48c66b8
Tomek Żebrowski <7668816-tototmek@users.noreply.gitlab.com>
2022-10-28 15:17:33 +0000

    ARC is not here anymore. Merging it to develop

--- src/web_interface/website/src/components/cameras/VideoDisplay.vue
+++ src/web_interface/website/src/components/cameras/VideoDisplay.vue
@@ -5,7 +5,11 @@
             :key="stream.data.id"
             :extraData="{ ...stream.data, removeIndex: index }"
         >
-            <VideoStream :ros="ros" :host="host" :stream="stream" />
+            <VideoStream
+                :host="host"
+                :stream="stream"
+                :peer="peers[stream.data.topic]"
+            />
         </DragItem>
         <div v-if="streams.length === 0" class="centered">
             Drag some streams here!
@@ -19,9 +23,9 @@ import VideoStream from '@/components/cameras/VideoStream.vue'
 export default {
     components: { DragItem, VideoStream },
     props: {
-        ros: Object, // should be a global
         host: String, // should be a global
         layout: Object,
+        peers: Object,
         rect: Object,
     },
 
--- src/web_interface/website/src/router/index.js
+++ src/web_interface/website/src/router/index.js
@@ -9,46 +9,46 @@ import Cameras from '../views/Cameras.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    props: true,
-  },
-  {
-    path: '/visualization',
-    name: 'Visualization',
-    component: Visualization,
-    props: true,
-    meta: {title: "Visualization",},
-  },
-  {
-    path: '/control',
-    name: 'Control',
-    component: Control,
-    props: true,
-    meta: {title: "Control",},
-  },
-  {
-    path: '/science',
-    name: 'Science',
-    component: Science,
-    props: true,
-    meta: {title: "Science",},
-  },
-  {
-    path: '/cameras',
-    name: 'Cameras',
-    component: Cameras,
-    props: true,
-    meta: {title: "Cameras",},
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        props: true,
+    },
+    {
+        path: '/visualization',
+        name: 'Visualization',
+        component: Visualization,
+        props: true,
+        meta: { title: 'Visualization' },
+    },
+    {
+        path: '/control',
+        name: 'Control',
+        component: Control,
+        props: true,
+        meta: { title: 'Control' },
+    },
+    {
+        path: '/science',
+        name: 'Science',
+        component: Science,
+        props: true,
+        meta: { title: 'Science' },
+    },
+    {
+        path: '/cameras',
+        name: 'Cameras',
+        component: Cameras,
+        props: true,
+        meta: { title: 'Cameras' },
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes,
 })
 
 export default router
