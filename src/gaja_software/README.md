500da0d8e040dbf1d98c9aee2f7a772ce558b298
tototmek <zebromek@gmail.com>
2023-01-03 18:23:17 +0100

    Add gaja_driver.py

---
+++ src/gaja_software/README.md
@@ -0,0 +1,55 @@
+# gaja_software package
+
+This package contains software for controlling the systems of Gaja rover, as well as the legacy software in `legacy_software` directory.
+
+## Usage
+
+### Launch files
+
+- `gaja_driver.launch`
+
+  ```bash
+  $ roslaunch gaja_software gaja_driver.launch
+  ```
+
+  Launches the gaja driver node.
+
+### Nodes
+
+- `gaja_driver`
+  
+  Description:
+
+  Sends serial commands to each of the robot's wheels based on received `geometry_msgs/Twist` messages.
+
+  Parameters:
+
+    - `~command_timeout`
+        - Type:         `float`
+        - Default:      `0.5`
+    
+    Time, after which a message becomes obsolete. If no messages are received since that one, the robot stops.
+
+    - `~l_x`
+        - Type:         `float`
+        - Default:      `0.13`
+
+    Half of the distance between the left and the right wheels (center to center).
+
+    - `~l_y`
+        - Type:         `float`
+        - Default:      `0.08`
+    
+    Half of the distance between the front and the rear wheels (axis to axis)
+
+    - `~r`
+        - Type:         `float`
+        - Default:      `0.045`
+    
+    Radius of the wheel (from wheel's axis to roller's axis)
+    
+  Subscribed topics:
+    
+    - `/cmd_vel`
+
+    Velocity commands for the rover.
---
+++ src/gaja_software/scripts/gaja_driver.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+import serial
+import rospy
+from geometry_msgs.msg import Twist
+
+class GajaDriver:
+    def __init__(self, portname, baudrate):
+        rospy.init_node('gaja_driver')
+        rospy.loginfo('gaja_driver started.')
+        self.sub = rospy.Subscriber('cmd_vel', Twist, self.cmd_vel_callback)
+        try:
+            self.serial = serial.Serial(portname, baudrate)
+        except serial.SerialException as e:
+            rospy.logerr("Could not open serial port %s: %s" % (portname, e))
+            exit(1)
+        self.twist = Twist()
+        self.last_command_time = rospy.Time.now()
+        self.command_timeout = rospy.Duration(
+            rospy.get_param('~command_timeout', 0.5))
+
+        # Load kinematic description of the robot
+        self.l_x = rospy.get_param('~l_x', 0.13)
+        self.l_y = rospy.get_param('~l_y', 0.08)
+        self.r = rospy.get_param('~r', 0.045)
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            wheel_cmd = self.calculate_command()
+            self.send_command(wheel_cmd)
+            rate.sleep()
+    
+    def cmd_vel_callback(self, msg):
+        # Stores the containts of command from /cmd_vel
+        self.twist.linear.x = msg.linear.x
+        self.twist.linear.y = msg.linear.y
+        self.twist.angular.z = msg.angular.z
+        self.last_command_time = rospy.Time.now()
+    
+    def calculate_command(self):
+
+        # Stops the robot if no messages have been received
+        if rospy.Time.now() - self.last_command_time > self.command_timeout:
+            self.twist = Twist()
+            rospy.logwarn("Command timeout")
+        
+        # Calculates wheels' velocities based on the command from /cmd_vel
+        l_x = 0.13
+        l_y = 0.08
+        r = 0.045
+
+        v_x = self.twist.linear.x
+        v_y = self.twist.linear.y
+        v_theta = self.twist.angular.z
+
+        v_l = (l_x + l_y) * v_theta
+
+        front_left = (v_x - v_y - v_l) / r
+        front_right = (v_x + v_y + v_l) / r
+        rear_left = (v_x + v_y - v_l) / r
+        rear_right = (v_x - v_y + v_l) / r
+
+        cmd = {
+            'front_left': clamp(int(127 * front_left), -127, 127),
+            'front_right': clamp(int(127 * front_right), -127, 127),
+            'rear_left': clamp(int(127 * rear_left), -127, 127),
+            'rear_right': clamp(int(127 * rear_right), -127, 127),
+        }
+        return cmd
+
+    def send_command(self, msg):
+        msg = " 128 0 %d 192 \n 128 1 %d 192 \n 128 2 %d 192 \n 128 3 %d 192" \
+            % (msg['front_left'], msg['front_right'], msg['rear_left'], msg['rear_right'])
+        rospy.logdebug("Sending via serial:\n"+msg)
+        self.serial.write(msg.encode('utf-8'))
+
+    def __del__(self):
+        self.serial.close()
+
+def clamp(value, min_value, max_value):
+    return max(min_value, min(max_value, value))
+
+if __name__ == '__main__':
+    try:
+        driver = GajaDriver('/dev/ttyUSB0', 9600)
+        driver.run()
+    except rospy.ROSInterruptException:
+        pass
