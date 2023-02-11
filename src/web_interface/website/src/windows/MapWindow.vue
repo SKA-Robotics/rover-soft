48cc3c640a2022a2828d18895241e7d7156c8f88
MaciejSzczepkowski <macko.szczepkowski@gmail.com>
2023-02-11 21:48:02 +0100

    pozycja skalowalna z rozmiarem mapy

--- src/web_interface/website/src/windows/MapWindow.vue
+++ src/web_interface/website/src/windows/MapWindow.vue
@@ -2,6 +2,11 @@
 import { defineProps, ref, onMounted, computed, onBeforeUnmount } from 'vue'
 
 const props = defineProps(['windowDimensions', 'extraConfig'])
+
+const map = ref(null)
+const image = ref(null)
+const interval = ref(null)
+
 const imgDim = ref({
     width: null,
     height: null,
@@ -12,12 +17,15 @@ let currentPos = ref({
     y: 100,
 })
 
-/*const roverPosition = computed(() => {
-    let mapHeight = dimensions.maxHeight
-    let mapWidth = dimensions.maxWidth
+let roverOnMapPos = ref({
+    x: null,
+    y: null,
+})
 
-
-})*/
+let scale = ref({
+    x: null,
+    y: null,
+})
 
 const dimensions = computed(() => {
     const aspectRatio = imgDim.value.width / imgDim.value.height
@@ -40,12 +48,31 @@ const dimensions = computed(() => {
     }
 })
 
-const map = ref(null)
-const image = ref(null)
-const interval = ref(null)
-const posInterval = ref(null)
-
 const draw = () => {
+    clearInterval(interval)
+    drawMap()
+    drawPosition()
+}
+
+onMounted(() => {
+    let img = new Image()
+    img.src = '/maps/mapa.jpg'
+
+    img.onload = () => {
+        image.value = img
+
+        imgDim.value.height = img.height
+        imgDim.value.width = img.width
+    }
+
+    interval.value = setInterval(draw, 10)
+})
+
+onBeforeUnmount(() => {
+    clearInterval(interval.value)
+})
+
+let drawMap = () => {
     if (map.value && image.value) {
         //console.log('w' + dimensions.value.width)
         //console.log('h' + dimensions.value.height)
@@ -57,38 +84,27 @@ const draw = () => {
             dimensions.value.width,
             dimensions.value.height
         )
+        scale.value.x = dimensions.value.height / imgDim.value.height
+        scale.value.y = dimensions.value.width / imgDim.value.width
     } else {
         console.log('brak obrazka')
     }
 }
 
-onMounted(() => {
-    let img = new Image()
-    img.src = '/maps/mapa.jpg'
-
-    img.onload = () => {
-        image.value = img
-        imgDim.value.height = img.height
-        imgDim.value.width = img.width
-    }
-
-    interval.value = setInterval(draw, 10)
-    posInterval.value = setInterval(drawPosition, 10)
-})
-
-onBeforeUnmount(() => {
-    clearInterval(interval.value)
-})
-
 let drawPosition = () => {
-    clearInterval(posInterval)
     let ctx = map.value.getContext('2d')
+
+    roverOnMapPos.value.x = currentPos.value.x * scale.value.x //TODO: make a function
+    roverOnMapPos.value.y = currentPos.value.y * scale.value.y
+
     console.log('kropka x' + currentPos.value.x)
     console.log('kropka y' + currentPos.value.y)
+    console.log('obliczone x' + roverOnMapPos.value.x)
+    console.log('obliczone y' + roverOnMapPos.value.y)
     ctx.beginPath()
     ctx.lineWidth = 3
     ctx.strokeStyle = 'red'
-    ctx.arc(currentPos.value.x, currentPos.value.y, 1, 1, Math.PI * 2)
+    ctx.arc(roverOnMapPos.value.x, roverOnMapPos.value.y, 1, 1, Math.PI * 2)
     ctx.stroke()
 }
 </script>
