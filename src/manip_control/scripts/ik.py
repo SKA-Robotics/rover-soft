36bcd1894384280350163dcff0b79a26b300d9a8
tototmek <zebromek@gmail.com>
2023-04-02 12:17:16 +0200

    Improve variable names, document reasoning behind ik

--- src/manip_control/scripts/ik.py
+++ src/manip_control/scripts/ik.py
@@ -63,70 +63,70 @@ class SiriusII_IKSolver(IKSolver):
 
         solution = self._initialize_solution()
         limits = self.limits
-        l = self.lengths
-        x = target.x
-        y = target.y
+        lengths = self.lengths
+        x_t = target.x
+        y_t = target.y
+
+        # The reasoning behind this code is explained in docs/Inverse_Kinematics_Formula.pdf
 
         # Last joint value simply equals to its target value
         solution[4] = target.roll
         # First joint angle can be easily obtained as it is
         # the only joint allowing for movement along y-axis
-        solution[0] = math.atan2(y, x)
+        solution[0] = math.atan2(y_t, x_t)
         if not checkBounds(solution[0], limits[0]):
             raise Exception("IK solution outside of joint limits!")
 
         # Project target position to manip plane
-        d = math.sqrt(x * x + y * y)
+        r_t = math.sqrt(x_t * x_t + y_t * y_t)
         # Translate, so link 0 length does not need to be considered
         # in formulas below
-        z = target.z - l[0]
+        z_t = target.z - lengths[0]
 
         # Find 3rd link's start position given its absolute angle
         # (target.alpha) and end position (target position)
         alpha = target.pitch
-        dp, zp = self._get_link3_startposition(l, d, z, alpha)
+        r_3, z_3 = self._get_link3_startposition(lengths, r_t, z_t, alpha)
 
         # Find 2nd link's start position
         # The geometry constraints lead to equation system with
         # two solutions. Due to physical constraints of the manipulator
         # only one of these solutions will be possible to reach
-        dpp, zpp = self._find_middlepoint_solution1(l, dp, zp)
+        r_2, z_2 = self._find_middlepoint_solution1(lengths, r_3, z_3)
         # Given the position, angles of the joints can be calculated
-        solution[1], solution[2], solution[3] = self._calculate_angles(target, dp, zp, dpp, zpp)
+        solution[1], solution[2], solution[3] = self._calculate_angles(r_3, z_3, r_2, z_2, alpha)
         # Check if the calculated angles are within manipulator's limits
         if self._angles_within_constraints(solution):
             return ManipJointState.from_list(solution)
 
         raise Exception("IK solution outside of joints limits")
 
-    def _get_link3_startposition(self, l, d, z, alpha):
-        dp = d - l[3] * math.cos(alpha)
-        zp = z + l[3] * math.sin(alpha)
-        return dp, zp
+    def _get_link3_startposition(self, lengths, r_t, z_t, alpha):
+        r_3 = r_t - lengths[3] * math.cos(alpha)
+        z_3 = z_t + lengths[3] * math.sin(alpha)
+        return r_3, z_3
 
     def _angles_within_constraints(self, solution):
         return all([checkBounds(solution[i], self.limits[i]) for i in range(1, len(solution))])
 
-    def _calculate_angles(self, target, dp, zp, dpp, zpp):
-        alpha = target.pitch
-        beta = math.atan2(zpp, dpp)
-        gamma = math.atan2(zpp - zp, dp - dpp)
+    def _calculate_angles(self, r_3, z_3, r_2, z_2, alpha):
+        beta = math.atan2(z_2, r_2)
+        gamma = math.atan2(z_2 - z_3, r_3 - r_2)
         angle1 = math.pi / 2 - beta
         angle2 = beta + gamma
         angle3 = alpha - gamma
         return angle1, angle2, angle3
 
-    def _find_middlepoint_solution1(self, l, dp, zp):
-        dpp = (dp * dp + l[1] * l[1] - l[2] * l[2] + zp * zp - (zp * (dp * math.sqrt(
-            (l[1] * l[2] * 2.0 - dp * dp + l[1] * l[1] + l[2] * l[2] - zp * zp) *
-            (l[1] * l[2] * 2.0 + dp * dp - l[1] * l[1] - l[2] * l[2] + zp * zp)) + (dp * dp) * zp + (l[1] * l[1]) * zp -
-                                                                      (l[2] * l[2]) * zp + zp * zp * zp)) /
-               (dp * dp + zp * zp)) / (dp * 2.0)
-        zpp = (dp * math.sqrt((l[1] * l[2] * 2.0 - dp * dp + l[1] * l[1] + l[2] * l[2] - zp * zp) *
-                              (l[1] * l[2] * 2.0 + dp * dp - l[1] * l[1] - l[2] * l[2] + zp * zp)) + (dp * dp) * zp +
-               (l[1] * l[1]) * zp - (l[2] * l[2]) * zp + zp * zp * zp) / ((dp * dp) * 2.0 + (zp * zp) * 2.0)
+    def _find_middlepoint_solution1(self, l, x, y):
+        r_2 = (x * x + l[1] * l[1] - l[2] * l[2] + y * y -
+               (y * (x * math.sqrt((l[1] * l[2] * 2.0 - x * x + l[1] * l[1] + l[2] * l[2] - y * y) *
+                                   (l[1] * l[2] * 2.0 + x * x - l[1] * l[1] - l[2] * l[2] + y * y)) + (x * x) * y +
+                     (l[1] * l[1]) * y - (l[2] * l[2]) * y + y * y * y)) / (x * x + y * y)) / (x * 2.0)
+        z_2 = (x * math.sqrt((l[1] * l[2] * 2.0 - x * x + l[1] * l[1] + l[2] * l[2] - y * y) *
+                             (l[1] * l[2] * 2.0 + x * x - l[1] * l[1] - l[2] * l[2] + y * y)) + (x * x) * y +
+               (l[1] * l[1]) * y - (l[2] * l[2]) * y + y * y * y) / ((x * x) * 2.0 + (y * y) * 2.0)
 
-        return dpp, zpp
+        return r_2, z_2
 
     def _initialize_solution(self):
         solution = [0] * len(self.limits)
