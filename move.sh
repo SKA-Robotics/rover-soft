43d7d8e8b6e582460ecc69f88fbe65ad506afb23
Tomek Zebrowski <zebromek@gmail.com>
2023-05-31 06:22:43 +0200

    Modify joystick mapping

--- move.sh
+++ move.sh
@@ -1,7 +1,8 @@
-rostopic pub -s -r10 /roboclaw_driver/129/motor2/set_joint_state sirius_msgs/JointState "header: auto
+rostopic pub -s -r10 /roboclaw_driver/130/motor2/set_joint_state sirius_msgs/JointState "header: auto
 position: []
 velocity: []
 acceleration: []
 effort: []
-duty: [10]" 
+duty: [15]" 
+
 
