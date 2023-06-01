5499f12bb9ad0f2bbd7b18fc9db30ab946a8a21e
Tomek Zebrowski <zebromek@gmail.com>
2023-06-01 04:31:44 +0200

    switch to socketcan_bridge package

--- src/sirius_modules/scripts/can_utils.py
+++ src/sirius_modules/scripts/can_utils.py
@@ -1,23 +1,22 @@
-import can
-import time
+import rospy
+from can_msgs.msg import Frame
 
 MAX_DATA_LENGTH = 8
 FRAMES_PER_STRING = 3
 MAX_STRING_LENGTH = FRAMES_PER_STRING * MAX_DATA_LENGTH
 
-can.rc['interface'] = 'socketcan'
-can.rc['channel'] = 'slcan0'
-can.rc['bitrate'] = 500000
-bus = can.interface.Bus()
 
-def send(arbitration_id, data):
+def make_message(arbitration_id, data) -> Frame:
     if len(data) > MAX_DATA_LENGTH:
         raise AssertionError(f"Data size exceeded! {len(data)}/{MAX_DATA_LENGTH}")
-    message = can.Message(arbitration_id=arbitration_id,
-                       is_extended_id =False,
-                       data=data)
-    bus.send(message)
-    time.sleep(0.005)
+    message = Frame()
+    message.header.stamp = rospy.Time.now()
+    message.id = arbitration_id
+    message.dlc = len(data)
+    while len(data) < MAX_DATA_LENGTH:
+        data.append(0x00)
+    message.data = data
+    return message
 
 def _string_to_bytes(text):
     return [ord(character) for character in text]
@@ -28,26 +27,20 @@ def _string_to_bytelists(text):
         _string_to_bytes(text[i*MAX_DATA_LENGTH:(i+1)*MAX_DATA_LENGTH])
         for i in range(FRAMES_PER_STRING)]
 
-def _make_string_messages(arbitration_ids, text):
+def _bytelists_to_messages(arbitration_ids, text):
     bytelists = _string_to_bytelists(text)
     return [
-        can.Message(arbitration_id=id, is_extended_id=False, data=bytes)
+        make_message(id, bytes)
         for bytes, id in zip(bytelists, arbitration_ids)]
     
-def send_string(arbitration_ids, message):
+def make_string_messages(arbitration_ids, message):
     if len(arbitration_ids) != FRAMES_PER_STRING:
         raise AssertionError(
             f"Must provide {FRAMES_PER_STRING} arbitration IDs. {len(arbitration_ids)} given.")
     if len(message) > MAX_STRING_LENGTH:
         raise AssertionError(
             f"String length limit exceeded! {len(message)}/{MAX_STRING_LENGTH}")
-    messages = _make_string_messages(arbitration_ids, message)
-    for message in messages:
-        bus.send(message)
-        time.sleep(0.005)
+    return _bytelists_to_messages(arbitration_ids, message)
 
 def bits_to_int(bits):
     return int(''.join(map(str, map(int, bits))), 2)
-
-def close():
-    bus.shutdown()
--- src/sirius_modules/scripts/led_flag.py
+++ src/sirius_modules/scripts/led_flag.py
@@ -5,17 +5,22 @@ import can_utils as can
 
 ARBITRATION_ID = 0x69
 STRING_ARBITRATION_IDS = [0x6a, 0x6b, 0x6c]
+CAN_SEND_TOPIC = "/sent_messages"
 
+publisher = rospy.Publisher(CAN_SEND_TOPIC, can.Frame, queue_size=10)
 
 def send_to_bus(msg: FlagState):
     bits = [msg.red, msg.green, msg.blue, msg.yellow, msg.buzzer]
-    can.send(ARBITRATION_ID, [can.bits_to_int(bits)])
+    m = can.make_message(ARBITRATION_ID, [can.bits_to_int(bits)])
+    publisher.publish(m)
     rospy.sleep(0.01)
-    can.send_string(STRING_ARBITRATION_IDS, msg.text)
+    msgs = can.make_string_messages(STRING_ARBITRATION_IDS, msg.text)
+    for m in msgs:
+        publisher.publish(m)
+        rospy.sleep(0.01)
     rospy.loginfo(
         f"Sending to LED Flag: rgby: {int(msg.red)}{int(msg.green)}{int(msg.blue)}{int(msg.yellow)},\
  buzzer: {('on' if msg.buzzer else 'off')}, text: \"{msg.text}\"")
-    rospy.sleep(0.01)
 
 
 rospy.init_node("led_flag")
