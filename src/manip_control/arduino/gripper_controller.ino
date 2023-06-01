cf527eb8dc24a8e0e402eaca7270c08062d50229
Tomek Zebrowski <zebromek@gmail.com>
2023-06-01 17:11:21 +0200

    Backup serial gripper controller

--- src/manip_control/arduino/gripper_controller.ino
+++ src/manip_control/arduino/gripper_controller.ino
@@ -5,27 +5,12 @@ void setup() {
   Serial.begin(9600);
 }
 
-int state = 0;
 int command = 0;
-int temp = 0;
 
 void loop() {
   int byte;
   while (Serial.available()) {
     byte = Serial.read();
-    switch(state) {
-      case 0:
-        if (byte == 128) state = 1;
-        break;
-      case 1:
-        temp = byte;
-        state = 2;
-        break;
-      case 2:
-        if (byte == 192) command = temp;
-        state = 0;
-        break;
-    }
   }
 
   analogWrite(PWM_PIN, command);
