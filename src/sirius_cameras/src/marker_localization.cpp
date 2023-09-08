76c6ac2964baa4bd78fa19121dbc0d10fccda0dd
Gabriel Brzezinski <gabriel.brzezinski.stud@pw.edu.pl>
2023-09-08 08:07:51 +0200

    Use Marker class instead of alvar

--- src/sirius_cameras/src/marker_localization.cpp
+++ src/sirius_cameras/src/marker_localization.cpp
@@ -14,21 +14,17 @@
 #include <tf2_ros/transform_listener.h>
 #include <ros/ros.h>
 #include <ArtagDetector.hpp>
-#include <tf_conversions/tf_eigen.h>
+#include <tf2_eigen/tf2_eigen.h>
 
 using namespace std;
 
-alvar::Camera* camera;
 ros::Publisher marker_publisher;
 ros::Publisher pose_publisher;
 tf2_ros::Buffer tf_buffer;
-alvar::MarkerDetector<alvar::MarkerData> marker_detector;
 ArtagDetector artag_detector;
 
 std::string base_link;
 std::string world_frame;
-double max_new_marker_error;
-double max_track_error;
 double max_error;
 void publishPose(const ar_track_alvar_msgs::AlvarMarkers& markers)
 {
@@ -70,83 +66,77 @@ void publishPose(const ar_track_alvar_msgs::AlvarMarkers& markers)
 
 void camera_callback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::CameraInfoConstPtr& camera_info)
 {
-    try
+  try
+  {
+    ar_track_alvar_msgs::AlvarMarkers alvar_markers;
+    geometry_msgs::TransformStamped cam_to_base_link;
+
+    auto cv_image = cv_bridge::toCvShare(image, sensor_msgs::image_encodings::BGR8);
+    auto markers = artag_detector.detect(cv_image->image);
+    if (markers.empty())
     {
-      ar_track_alvar_msgs::AlvarMarkers alvar_markers;
-      geometry_msgs::TransformStamped cam_to_base_link;
+      return;
+    }
 
-      auto cv_image_copy = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
-      marker_detector.Detect(cv_image_copy->image, camera, true, false, max_new_marker_error, max_track_error);
-
-      auto cv_image = cv_bridge::toCvShare(image, sensor_msgs::image_encodings::BGR8);
-      auto markers = artag_detector.detect(cv_image->image);
-
-      auto camera_tf = image->header.frame_id;
-      if (!base_link.empty() || camera_tf.substr(camera_tf.length() - 7) == "_optical")
+    auto camera_tf = image->header.frame_id;
+    if (!base_link.empty() || camera_tf.substr(camera_tf.length() - 7) == "_optical")
+    {
+      try
       {
-        try
+        if (base_link.empty())
         {
-          if (base_link.empty())
-          {
-            base_link = camera_tf.substr(0, camera_tf.length() - 7);
-          }
-          cam_to_base_link = tf_buffer.lookupTransform(base_link, image->header.frame_id, image->header.stamp);
+          base_link = camera_tf.substr(0, camera_tf.length() - 7);
         }
-        catch (tf2::TransformException ex)
-        {
-          ROS_WARN("%s", ex.what());
-          return;
-        }
+        cam_to_base_link = tf_buffer.lookupTransform(base_link, image->header.frame_id, image->header.stamp);
       }
-      else
+      catch (tf2::TransformException ex)
       {
-        cam_to_base_link = geometry_msgs::TransformStamped();
-        cam_to_base_link.transform.rotation.w = 1;
-      }
-
-      if (markers.empty())
-      {
+        ROS_WARN("%s", ex.what());
         return;
       }
-
-      for (auto& marker : markers)
-      {
-        tf2::Vector3 origin(marker.position.x(), marker.position.y(), marker.position.z());
-        tf2::Quaternion rotation(marker.orientation.x(), marker.orientation.y(), marker.orientation.z(),
-                                 marker.orientation.w());
-
-        tf2::Transform marker_in_camera_tf(rotation, origin);
-
-        tf2::Stamped<tf2::Transform> cam_to_base_link_tf;
-        tf2::fromMsg(cam_to_base_link, cam_to_base_link_tf);
-        tf2::Transform marker_in_base_link_tf = cam_to_base_link_tf * marker_in_camera_tf;
-
-        ar_track_alvar_msgs::AlvarMarker ar_pose_marker;
-        tf2::toMsg(marker_in_base_link_tf, ar_pose_marker.pose.pose);
-        ar_pose_marker.header.frame_id = base_link;
-        ar_pose_marker.header.stamp = image->header.stamp;
-        ar_pose_marker.id = marker.id;
-
-        auto error = marker.error.x();
-        ar_pose_marker.confidence = 1.0 - std::min(error, max_error) / max_error;
-
-        alvar_markers.markers.push_back(ar_pose_marker);
-      }
-
-      alvar_markers.header.stamp = image->header.stamp;
-      alvar_markers.header.frame_id = base_link;
-      marker_publisher.publish(alvar_markers);
-      publishPose(alvar_markers);
     }
-    catch (const std::exception& e)
+    else
     {
-      ROS_ERROR("%s", e.what());
+      cam_to_base_link = geometry_msgs::TransformStamped();
+      cam_to_base_link.transform.rotation.w = 1;
     }
+
+    for (auto& marker : markers)
+    {
+      auto marker_in_camera = tf2::eigenToTransform(marker.transform);
+      geometry_msgs::TransformStamped marker_in_base_link;
+      tf2::doTransform(marker_in_camera, marker_in_base_link, cam_to_base_link);
+
+      ar_track_alvar_msgs::AlvarMarker ar_pose_marker;
+      tf2::Transform marker_in_base_link_tf;
+      tf2::fromMsg(marker_in_base_link.transform, marker_in_base_link_tf);
+      tf2::toMsg(marker_in_base_link_tf, ar_pose_marker.pose.pose);
+      ar_pose_marker.header.frame_id = base_link;
+      ar_pose_marker.header.stamp = image->header.stamp;
+      ar_pose_marker.id = marker.id;
+
+      auto error = marker.error.x();
+      ar_pose_marker.confidence = 1.0 - std::min(error, max_error) / max_error;
+
+      alvar_markers.markers.push_back(ar_pose_marker);
+    }
+
+    alvar_markers.header.stamp = image->header.stamp;
+    alvar_markers.header.frame_id = base_link;
+    marker_publisher.publish(alvar_markers);
+    publishPose(alvar_markers);
   }
+  catch (const std::exception& e)
+  {
+    ROS_ERROR("%s", e.what());
+  }
+}
 
 int main(int argc, char* argv[])
 {
   double marker_size;
+  double max_new_marker_error;
+  double max_track_error;
   double update_rate;
 
   ros::init(argc, argv, "marker_localization");
@@ -161,13 +151,11 @@ int main(int argc, char* argv[])
   nh.param<std::string>("world_frame", world_frame, "map");
   max_error = std::max(max_new_marker_error, max_track_error);
 
-  marker_detector.SetMarkerSize(marker_size * 100);
   marker_publisher = nh.advertise<ar_track_alvar_msgs::AlvarMarkers>("markers", 0);
   pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("pose", 0);
 
   image_transport::ImageTransport it(nh);
   auto camera_subscriber = it.subscribeCamera("image", 1, &camera_callback);
-  camera = new alvar::Camera(nh, camera_subscriber.getInfoTopic());
   artag_detector.init(nh, camera_subscriber.getInfoTopic(), marker_size, max_new_marker_error, max_track_error);
 
   ros::Rate rate(update_rate);
