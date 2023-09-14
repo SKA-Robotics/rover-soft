0ee384ee7785c860c4c4346e55dc5cc3177d083a
sirius2 <ska.robotics@gmail.com>
2023-09-14 23:29:00 +0200

    Automatic can start and fix to power measurement

--- launch_from_service.sh
+++ launch_from_service.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-source "/home/rover/.bashrc"
+slcand -o -s3 -t hw -S 115200 /dev/ttyACM0
+ip link set up slcan0
 
-#TERM=xterm-256color tmux new-session -d -s ros -n roscore "bash -l -i -c \"roscore\"" 
-#sleep 10 
-#TERM=xterm-256color tmux new-window -d -t ros: -n nodes "bash -l -i -c \"source /home/jetson/rover-soft/devel/setup.bash; rosrun rosmon_core rosmon sirius_description sirius2.launch\""
+sudo -i -u rover bash << EOF
+
+source "/home/rover/.bashrc"
 
 TERM=xterm-256color tmux new-session -d -s ros -n nodes "bash -l -i -c \"source /home/rover/rover-soft/devel/setup.bash; roslaunch sirius_description sirius2.launch\""
 TERM=xterm-256color tmux new-window -d -t ros: -n web_server "source /home/rover/.nvm/nvm.sh; cd /home/rover/robot-web-interface/server; npm run start; bash -i"
+TERM=xterm-256color tmux new-window -d -t ros: -n cameras "sudo tee /sys/module/uvcvideo/parameters/bandwidth_cap; docker compose run gstreamer-cameras; bash -i"
+TERM=xterm-256color tmux new-window -d -t ros: -n power_readings "source /home/rover/rover-soft/devel/setup.bash; until rostopic list ; do sleep 1; done; rostopic echo -p /power_status | tee /home/rover/power_readings/$(date +"%Y-%m-%dT%H:%M:%S%z"); bash -i"
+
+EOF
--- stop_from_service.sh
+++ stop_from_service.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+sudo -i -u rover bash << EOF
+
 source "/home/rover/.bashrc"
 
 tmux kill-session -t ros
 
+EOF
